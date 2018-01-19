afff0b76d0bc9a9a6ef3be18e12955b2e96d51b0
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2018-01-20 01:25:08 +0800

    started on logic for calculator

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,5 +1,26 @@
 
 $(document).ready(function(){
-    
-    
+
+    var theParent = document.querySelector("#calculator");
+    theParent.addEventListener("click", getButton, false);
+    //var input = document.getElementById("screen");
+     
+    function getButton(e) {
+        if (e.target !== e.currentTarget) {
+
+            //alert("Hello " + clickedItem);
+            //alert("hello" + clickedValue);            
+            //var clickedItem = e.target.id;
+            var clickedValue = e.target.value;            
+            var screenInput = document.getElementById('screen');
+            screenInput.value += clickedValue;
+        }
+        e.stopPropagation();
+    }
+  
 })
+
+
+
+
+
